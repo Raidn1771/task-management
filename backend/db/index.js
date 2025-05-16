029356e5ed16dbb3a316290ccc5fa9145c4369b2
RishenJessilFigarez <61039771+Raidn1771@users.noreply.github.com>
2025-05-16 10:45:55 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/TODO-DB',
+      process.env.MONGODB_URL || 'mongodb://localhost:27017/TODO-DB',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
