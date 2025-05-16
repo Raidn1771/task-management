dfa91e9bde3448cd50946894fa03d1bfc2e28b50
RishenJessilFigarez <61039771+Raidn1771@users.noreply.github.com>
2025-05-16 11:11:59 +0530

    Update axios.js

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://task-management-backend-ehcv.onrender.com',
+  baseURL: 'https://task-management-backend-ehcv.onrender.com/api',
 });
 
 API.interceptors.request.use(req => {
